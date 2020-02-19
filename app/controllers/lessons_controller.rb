9c0b7b6893ab974902384cbf7691a5cca40ecb19
Dedrick Harrington <dedrick33@live.com>
2020-02-19 00:27:02 +0000

    Finished

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true, with: :exception
   before_action :authenticate_user!
   before_action :require_authorized_for_lessons
 
