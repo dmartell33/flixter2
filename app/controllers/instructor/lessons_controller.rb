49629f279321232b556fa10ada7ae498768201ef
Dedrick Harrington <dedrick33@live.com>
2020-02-26 04:25:41 +0000

    Update

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,5 @@
 class Instructor::LessonsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  protect_from_forgery prepend: true, with: :exception
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
